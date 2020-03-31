29417844b67df954f503d60c0c00b50ddddd84c8
Matthew Howard <mattalanhoward@gmail.com>
2020-03-31 15:16:51 +0200

    Add 'CompareDNA' method and run test.

--- main.js
+++ main.js
@@ -30,31 +30,55 @@ const pAequorFactory = (specNum, dnaArr) => {
         dna:dnaArr,
         mutate() {
           const randomInx = Math.floor(Math.random() * 15)
-          console.log(`Random Index: ${randomInx}`)
+          // console.log(`Random Index: ${randomInx}`)
           let newBase = '';
               do {
                 newbase = returnRandBase();
               } while (newbase === this.dna[randomInx]);
               return this.dna[randomInx] = newbase;
-             }
+},
+        compareDNA(obj) {
+          let baseCounter = 0;
+          let percentSame = 0;
+          for(let i = 0; i<15; i ++) {
+            if (this.dna[i] === obj[i]) {
+              baseCounter ++;
+            }
           }
-  };
+          percentSame = ((baseCounter/15) * 100).toFixed(2);
+          return `The two DNA strands have ${percentSame}% DNA in common.`;
+         },
         
+      }
+  
+  };   
+  
+
+//run tests.  
+let test = pAequorFactory(1, mockUpStrand());
+console.log(test.specimenNum);
+console.log(test.dna);
+console.log(test.compareDNA([
+  'A', 'C', 'G', 'T',
+  'G', 'A', 'C', 'A',
+  'G', 'G', 'A', 'T',
+  'T', 'C', 'A'
+]))
 
 
 
-const specimen1 = pAequorFactory(counter.incrementCounter(),mockUpStrand())
-console.log(specimen1)
-specimen1.mutate()
-console.log(specimen1)
+// create Specimen 1 and show mutation.
+  // const specimen1 = pAequorFactory(counter.incrementCounter(),mockUpStrand())
+  // console.log(specimen1)
+  // specimen1.mutate()
+  // console.log(specimen1)
+  
+  // create Specimen 2 and show mutation
+  // const specimen2 = pAequorFactory(counter.incrementCounter(),mockUpStrand())
+  // console.log(specimen2)
+  // specimen2.mutate()
+  // console.log(specimen2)
 
-
-const specimen2 = pAequorFactory(counter.incrementCounter(),mockUpStrand())
-console.log(specimen2)
-specimen2.mutate()
-console.log(specimen2)
-
-
-
-
-
+  // Compare DNA bases of Specimen 1 and Specimen 2
+  // const compare = specimen1.compareDNA(specimen2)
+  // console.log(compare)
